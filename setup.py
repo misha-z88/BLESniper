90620b96db7eb4bef87dddb309f792ad322ee923
Misha / Mikayil <148778675+misha-z88@users.noreply.github.com>
2025-01-25 10:16:04 +0400

    Fix

--- setup.py
+++ setup.py
@@ -1,38 +1,21 @@
 from setuptools import setup, find_packages
-import sys
-
-requirements = [
-    'pybluez>=0.30.0',
-    'pyserial>=3.5'
-]
-
-if sys.platform == 'darwin':
-    requirements.append('pyobjc-framework-IOBluetooth')
-elif sys.platform == 'win32':
-    requirements.extend([
-        'pywin32',
-        'pypiwin32'
-    ])
 
 setup(
-    name='blestrike',
-    version='0.1.1',
+    name='blestrike', 
+    version='0.1.0',
     packages=find_packages(),
-    install_requires=requirements,
-    extras_require={
-        'macos': ['pyobjc-framework-IOBluetooth'],
-        'windows': ['pywin32', 'pypiwin32']
-    },
-    author='Security Research Team',
-    description='Cross-platform Bluetooth Analysis Toolkit',
+    install_requires=[
+        'pybluez==0.22',  
+        'pyserial>=3.5'
+    ],
+    author='Your Name',
+    description='Bluetooth Security Toolkit',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/misha-z88/BLEStrike',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Security',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3.7+'
     ]
 )
